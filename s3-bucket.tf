6395468cb929f8dc460dc8fd7f61060868948a14
Ryoji Fukuda <fukuda@braveridge.com>
2022-06-19 22:49:30 +0900

    fix bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  name          = "hashicat-aws"
+  bucket        = "hashicat-bucket"
   source        = "app.terraform.io/ryoqn-training/s3-bucket/aws"
   version       = "2.2.0"
   bucket_prefix = var.prefix
