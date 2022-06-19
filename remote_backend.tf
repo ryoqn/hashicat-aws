8e9b6c2c0104050dcde11ada349e318d39153882
Ryoji Fukuda <fukuda@braveridge.com>
2022-06-19 18:39:43 +0900

    Fixed typo

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "ryoqn-traning"
+    organization = "ryoqn-training"
     workspaces {
       name = "hashicat-aws"
     }
